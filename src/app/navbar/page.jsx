a649ad7895540ac2b4fafe951bfec2c01a949c7b
SahilPaul77 <Sahilpaul77@gmail.com>
2026-04-11 17:41:26 +0530

    location changed

--- src/app/navbar/page.jsx
+++ src/app/navbar/page.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           href="/"
           className="text-lg md:text-xl font-serif tracking-tighter uppercase font-bold"
         >
-          brightsmile
+         Dental World
         </Link>
 
         {/* Desktop Menu */}
